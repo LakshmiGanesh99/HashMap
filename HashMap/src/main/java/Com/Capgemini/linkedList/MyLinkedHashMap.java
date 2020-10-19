7b31ad085e91176a3a4fbd55a55c69d26b1a47da
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-20 00:07:32 +0530

    [LakshmiGanesh]add:ability to remove by the key

--- HashMap/src/main/java/Com/Capgemini/linkedList/MyLinkedHashMap.java
+++ HashMap/src/main/java/Com/Capgemini/linkedList/MyLinkedHashMap.java
@@ -29,6 +29,15 @@ public class MyLinkedHashMap<K,V> {
 			myMapNode.setValue(value);
 		}
 	}
+	
+	public K remove(K key) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		if(myLinkedList == null)
+			return null;
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.remove(key);
+		return (myMapNode == null) ? null : myMapNode.getKey();
+	}
 
 	public V get(K key) {
 		int index = this.getBucketIndex(key);
@@ -46,4 +55,3 @@ public class MyLinkedHashMap<K,V> {
 	}
 	
 }
- 
--- HashMap/src/test/java/Com/Capgemini/linkedList/MyLinkedHashMapTest.java
+++ HashMap/src/test/java/Com/Capgemini/linkedList/MyLinkedHashMapTest.java
@@ -1,9 +1,7 @@
-package Com.Capgenimi.linkedList;
+package Com.Capgemini.linkedList;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import Com.Capgemini.linkedList.MyLinkedHashMap;
 
 public class MyLinkedHashMapTest {
 	
@@ -15,7 +13,7 @@ public class MyLinkedHashMapTest {
         MyLinkedHashMap<String,Integer> myLinkedHashMap = new MyLinkedHashMap<>();
         String[] words = sentence.toLowerCase().split(" ");
         for(String word : words) {
-            Integer value =  myLinkedHashMap.get(word); 
+            Integer value =  myLinkedHashMap.get(word);
             if(value == null)
                 value = 1;
             else 
@@ -26,4 +24,24 @@ public class MyLinkedHashMapTest {
         int frequency = myLinkedHashMap.get("paranoid");
         Assert.assertEquals(3, frequency);
     }
+	
+	@Test
+	public void givenASentence_WhenAWordIsGiven_ShouldRemoveThatWordFromTheHashTable() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but"
+				+ " because they keep putting themselves deliberately into paranoid "
+				+ "avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value =  myLinkedHashMap.get(word);
+			if(value == null)
+				value = 1;
+			else 
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		String remove = myLinkedHashMap.remove("avoidable");
+		System.out.println(myLinkedHashMap);		
+		Assert.assertEquals("avoidable", remove);
+	}
 }
